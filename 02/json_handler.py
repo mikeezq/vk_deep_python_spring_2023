d6bfddf10a7319ad0ecd351ca69bfccb7e7956e1
Mikhail Kornilov <korn-m@yandex-team.ru>
2023-04-12 23:47:22 +0300

    fix hw2

---
+++ 02/json_handler.py
@@ -0,0 +1,24 @@
+import json
+
+from typing import List
+
+
+def parse_json(json_str: str, keyword_callback, required_fields: List[str] = None, keywords: List[str] = None) -> json:
+    try:
+        f_json = json.loads(json_str)
+    except json.decoder.JSONDecodeError:
+        return None
+
+    if not required_fields or not keywords or not keyword_callback:
+        return f_json
+
+    for key, _ in f_json.items():
+        if key in required_fields:
+            for word in f_json[key].split():
+                if word in keywords:
+                    change_keywords(key, word)
+    return f_json
+
+
+def change_keywords(required_field: str, keyword: str) -> str:
+    return f"{required_field} + {keyword}"
---
+++ 02/test_json_handler.py
@@ -0,0 +1,62 @@
+import json
+import functools
+
+from unittest import TestCase
+from faker import Faker
+from mock import patch
+
+from json_handler import parse_json
+
+
+class TestJsonHandler(TestCase):
+    def test_parse_calls(self):
+        json_str = '{"keyword1": "cat dog bad", "keyword2": "cat dog test"}'
+
+        with patch("json_handler.change_keywords") as mock_changer:
+            parse_json_call = functools.partial(parse_json, json_str, mock_changer)
+            parse_json_call(["keyword1", "keyword2"], ["cat", "test"])
+            parse_json_call(["keyword1"], ["cat"])
+            parse_json_call(["keyword2"], ["cat"])
+            parse_json_call(["keyword2"], ["fake"])
+
+            expected_calls1 = [("keyword1", "cat"), ("keyword2", "cat"), ("keyword2", "test"),
+                               ("keyword1", "cat"),
+                               ("keyword2", "cat")]
+
+            i = 0
+            for call in mock_changer.call_args_list:
+                for args in call:
+                    if args:
+                        assert args == expected_calls1[i]
+                        i += 1
+            assert mock_changer.call_count == 5
+
+        json_sets = fake_jsons()
+        with patch("json_handler.change_keywords") as mock_changer:
+            for json_set in json_sets:
+                parse_json(json.dumps(json_set), mock_changer, ["name"], ["test"])
+            self.assertEqual(mock_changer.call_count, 100)
+
+    @patch("json_handler.change_keywords")
+    def test_incorrect_jsons(self, mock_changer):
+        self.assertIsNone(parse_json("", mock_changer))
+        self.assertIsNone(parse_json('{"keyword1": "cat}', mock_changer))
+        self.assertIs(mock_changer.called, False)
+        self.assertEqual(parse_json('{"keyword1": "cat"}', None, [], []), {"keyword1": "cat"})
+        self.assertEqual(parse_json('{"keyword1": "cat"}', mock_changer, None), {"keyword1": "cat"})
+        self.assertEqual(parse_json('{"keyword1": "cat"}', mock_changer, ["some_key"], None), {"keyword1": "cat"})
+
+
+def fake_jsons():
+    fake = Faker(locale="Ru_ru")
+    json_sets = []
+    for _ in range(100):
+        doc = {
+            'name': 'test',
+            'address': fake.address(),
+            'company': fake.company(),
+            'country': fake.country(),
+            'text': fake.sentence()
+        }
+        json_sets.append(doc)
+    return json_sets
