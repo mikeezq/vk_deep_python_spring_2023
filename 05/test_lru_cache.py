7255dc50c17c29a578d888c055f7145db9cb7f79
Mikhail Kornilov <korn-m@yandex-team.ru>
2023-05-07 15:58:04 +0300

    fix hw5

--- 05/test_lru_cache.py
+++ 05/test_lru_cache.py
@@ -4,25 +4,38 @@ from lru_cache import LRUCache
 
 
 class TestLruCache(TestCase):
-    def setUp(self):
-        self.cache = LRUCache(2)
+    def test_lru_cache_2(self):
+        cache = LRUCache(2)
 
-    def test_lru_cache(self):
-        self.cache.set("k1", "val1")
-        self.cache.set("k2", "val2")
+        cache.set("k1", "val1")
+        cache.set("k2", "val2")
 
-        assert self.cache.get("k2") == "val2"
-        assert self.cache.get("k3") is None
+        assert cache.get("k3") is None
+        assert cache.get("k2") == "val2"
+        assert cache.get("k1") == "val1"
 
-        self.cache.set("k3", "val3")
-        assert self.cache.get("k1") is None
-        assert self.cache.get("k2") == "val2"
-        assert self.cache.get("k3") == "val3"
+        cache.set("k3", "val3")
 
-        self.cache.set("k2", "new_val2")
-        assert self.cache.get("k2") == "new_val2"
+        assert cache.get("k3") == "val3"
+        assert cache.get("k2") is None
+        assert cache.get("k1") == "val1"
 
-        self.cache.set("k4", "val4")
-        assert self.cache.get("k4") == "val4"
+        cache.set("k1", "new_val")
+        cache.set("k4", "val4")
 
-        assert self.cache.get("k3") is None
+        assert cache.get("k3") is None
+        assert cache.get("k1") == "new_val"
+        assert cache.get("k4") == "val4"
+
+    def test_lru_cache_1(self):
+        cache = LRUCache(1)
+
+        cache.set("k1", "val1")
+        cache.set("k1", "val2")
+
+        assert cache.get("k1") == "val2"
+
+        cache.set("k2", "val2")
+
+        assert cache.get("k1") is None
+        assert cache.get("k2") == "val2"
