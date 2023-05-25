2bdb0646356c35b917675348c285f0b40194752e
Mikhail Kornilov <korn-m@yandex-team.ru>
2023-05-25 22:12:54 +0300

    fix hw7

--- 07/test_urls_parser.py
+++ 07/test_urls_parser.py
@@ -1,29 +1,42 @@
+import argparse
 from unittest import mock
 
 from unittest.mock import AsyncMock
 
+import asyncio
 import pytest
 
+from asynctest import patch
 from aiounittest import AsyncTestCase
-
-from urls_parser import fetch_urls
+from urls_parser import fetch_urls, fetch_url
 
 
 class TestUrlsParser(AsyncTestCase):
     async def test_async_parse(self):
         async_mock = AsyncMock()
         with mock.patch("urls_parser.fetch_url", side_effect=async_mock) as mock_fetch_url:
-            await fetch_urls([5, "test_urls.txt"])
+            await fetch_urls(argparse.Namespace(c=5, file="test_urls.txt"))
             assert mock_fetch_url.call_count == 3
 
         async_mock = AsyncMock()
         with mock.patch("urls_parser.fetch_url", side_effect=async_mock) as mock_fetch_url:
-            await fetch_urls([10, "random_urls.txt"])
+            await fetch_urls(argparse.Namespace(c=10, file="random_urls.txt"))
             assert mock_fetch_url.call_count == 100
 
     async def test_incorrect_args(self):
-        with pytest.raises(ValueError, match="First argument must be numeric"):
-            await fetch_urls(["test", "test"])
-
         with pytest.raises(FileNotFoundError, match="File test does not exists"):
-            await fetch_urls([1, "test"])
+            await fetch_urls(argparse.Namespace(c=1, file="test"))
+
+    @patch("aiohttp.ClientSession.get")
+    async def test_fetch_url(self, mock_get):
+        test_url = "http://example.com"
+        response_text = "Response Text"
+
+        mock_response = AsyncMock()
+        mock_response.status = 200
+        mock_response.text.return_value = response_text
+
+        mock_get.return_value.__aenter__.return_value = mock_response
+
+        text = await fetch_url(test_url, asyncio.Semaphore(1))
+        assert text == response_text
--- 07/urls_parser.py
+++ 07/urls_parser.py
@@ -1,42 +1,49 @@
-import sys
 import os
 import asyncio
+import argparse
+import aiofiles
 import aiohttp
 
 
-COUNT = 0
-
-
 async def fetch_url(url, sem):
-    global COUNT
-
     async with aiohttp.ClientSession() as session:
         async with sem:
             async with session.get(url) as resp:
-                COUNT += 1
                 assert resp.status == 200
+                response_text = await resp.text()
+                return response_text
 
 
 async def fetch_urls(args):
-    try:
-        coroutines_count = int(args[0])
-    except ValueError:
-        raise ValueError("First argument must be numeric")
-
-    file = args[1]
+    file = args.file
     if not os.path.exists(file):
         raise FileNotFoundError(f"File {file} does not exists")
 
-    sem = asyncio.Semaphore(coroutines_count)
+    sem = asyncio.Semaphore(args.c)
 
-    tasks = [
-        asyncio.create_task(fetch_url(url.strip(), sem))
-        for url in open(file, "r")
-    ]
-    await asyncio.gather(*tasks)
-    return COUNT
+    tasks = []
+    async with aiofiles.open(file, "r") as f:
+        async for line in f:
+            url = line.strip()
+            task = asyncio.create_task(fetch_url(url, sem))
+            tasks.append(task)
+            if len(tasks) >= args.c:
+                await asyncio.gather(*tasks)
+                tasks = []
+
+    if tasks:
+        await asyncio.gather(*tasks)
+
+
+def parse_args():
+    args = argparse.ArgumentParser()
+
+    args.add_argument("-c", type=int, default=5, help="Number of concurrent requests")
+    args.add_argument("file", metavar="FILENAME")
+
+    return args.parse_args()
 
 
 if __name__ == "__main__":
-    sys_args = sys.argv[1:]
-    asyncio.run(fetch_urls(sys_args))
+    args = parse_args()
+    asyncio.run(fetch_urls(args))
